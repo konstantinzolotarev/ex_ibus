166a3524b3bb573f2203378177c101dfe85edb6c
Konstantin Zolotarev <konstantin.zolotarev@gmail.com>
2018-01-18 15:36:03 +0200

    Updated mix

--- mix.exs
+++ mix.exs
@@ -1,13 +1,22 @@
 defmodule ExIbus.Mixfile do
   use Mix.Project
 
+  @description """
+  Ibus helper modules
+  """
+
   def project do
     [
       app: :ex_ibus,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.3",
+      name: "Ibus protocol helper",
+      description: @description,
+      docs: [extras: ["README.md"]],
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      package: package(),
+      deps: deps(),
+      source_url: "https://github.com/konstantinzolotarev/ex_ibus"
     ]
   end
 
@@ -18,11 +27,17 @@ defmodule ExIbus.Mixfile do
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:ex_doc, "~> 0.14", only: :dev}
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Konstantin Zolotarev"],
+      licenses: ["MIT"],
+      links: %{"Github" => "https://github.com/konstantinzolotarev/ex_ibus"}
     ]
   end
 end
